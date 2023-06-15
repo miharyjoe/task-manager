81b2b0a27dda8ef8f71c58fbe8b406688bcc8cc7
miharyjoel <miharyjoel@gmail.com>
2023-06-15 10:23:24 +0300

    feat: add serverTime and diffTime

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,59 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-
+import { useEffect, useState } from "react";
 /**
   Calculates the time difference between the server time and client time.
   @param {Date} serverTime - The server time.
   @param {Date} clientTime - The client time.
   @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
 */
-const calculateTimeDifference = (server: Date, client: Date) => {};
 
+//To calculate the diff of the server time and client Time
+const calculateTimeDifference = (server: Date, client: Date): string => {
+  const diff = Math.abs(server.getTime() - client.getTime());
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-export default function Home() {
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+};
+
+interface HomeProps {
+  serverTime: string;
+}
+
+export default function Home({ serverTime }: HomeProps) {
   const router = useRouter();
+  // By initializing clientTime with new Date(serverTime),
+  // the client-side timer starts with the same value as the server time.
+  // This helps to avoid the mismatch in the initial rendered HTML
+  // and resolves the warning.
+  const [clientTime, setClientTime] = useState(new Date(serverTime));
+
+  //we add setInterval to the useEffect to update the clientTime every second.
+  //The client time is updated every second.
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setClientTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   const moveToTaskManager = () => {
     router.push("/tasks");
-  }
+  };
+  const formattedServerTime = new Date(serverTime).toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const timeDiff = calculateTimeDifference(new Date(serverTime), clientTime);
+
   return (
     <>
       <Head>
@@ -29,13 +68,11 @@ export default function Home() {
           {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
-          </p>
-
+            <span className="serverTime">{formattedServerTime}</span>
+          </p>{" "}
           {/* Display here the time difference between the server side and the client side */}
           <p>
-            Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Time diff: <span className="serverTime">{timeDiff}</span>
           </p>
         </div>
 
@@ -45,4 +82,14 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  // Get the current server time
+  const serverTime = new Date().toISOString();
+  return {
+    props: {
+      serverTime,
+    },
+  };
 }
