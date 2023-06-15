fae7a974011545968c91fbf459187ede0b54cac8
miharyjoel <miharyjoel@gmail.com>
2023-06-15 11:57:28 +0300

    feat: add useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,27 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from "react";
 
-}
+const isLocalStorageAvailable =
+  typeof window !== "undefined" && window.localStorage;
 
-export {
-  useLocalStorage
-}
+const useLocalStorage = <T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] => {
+  const [value, setValue] = useState<T>(() => {
+    if (isLocalStorageAvailable) {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    }
+    return initialValue;
+  });
+
+  useEffect(() => {
+    if (isLocalStorageAvailable) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export { useLocalStorage };
