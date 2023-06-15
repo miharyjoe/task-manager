a3651568bfb2a6bf668e0e6189d7ef780bf84686
miharyjoel <miharyjoel@gmail.com>
2023-06-15 11:07:36 +0300

    feat: add useTaskManager

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,41 @@
-const useTaskManager = () => {
+import create, { State } from "zustand";
 
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-export {
-  useTaskManager
+interface TaskManagerState extends State {
+  tasks: Task[];
+  searchTask: string;
+  addTask: (newTask: Task) => void;
+  updateTask: (taskId: number, updatedTask: Partial<Task>) => void;
+  deleteTask: (taskId: number) => void;
+  setSearchTask: (searchValue: string) => void;
 }
+
+const useTaskManager = create<TaskManagerState>((set) => ({
+  tasks: [],
+  searchTask: "",
+  addTask: (newTask: Task) =>
+    set((state) => ({
+      tasks: [...state.tasks, newTask],
+    })),
+  updateTask: (taskId: number, updatedTask: Partial<Task>) =>
+    set((state) => ({
+      tasks: state.tasks.map((task) =>
+        task.id === taskId ? { ...task, ...updatedTask } : task
+      ),
+    })),
+  deleteTask: (taskId: number) =>
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== taskId),
+    })),
+  setSearchTask: (searchValue: string) =>
+    set(() => ({
+      searchTask: searchValue,
+    })),
+}));
+
+export { useTaskManager };
